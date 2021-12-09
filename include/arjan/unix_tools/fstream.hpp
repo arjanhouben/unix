9b76f781e5d4f9ba90acf5983992a38fb49af782
Arjan Houben <a.houben@elitac.nl>
2021-12-09 17:42:22 +0100

    fstream constructors need not be explicit as their accepting types manage the resource

--- include/arjan/unix_tools/fstream.hpp
+++ include/arjan/unix_tools/fstream.hpp
@@ -10,10 +10,10 @@ struct basic_fstream : stream_base
 {
 	using streambuf_type = basic_streambuf< typename stream_base::char_type >;
 
-	explicit basic_fstream( file f ) :
+	basic_fstream( file f ) :
 		basic_fstream( streambuf_type( std::move( f ) ) ) {}
 
-	explicit basic_fstream( streambuf_type buf ) :
+	basic_fstream( streambuf_type buf ) :
 		stream_base( &buffer_ ),
 		buffer_( std::move( buf ) ) {}
 	
